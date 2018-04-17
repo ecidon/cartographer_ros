96b8f242ca55e5d8ae72e4f5349e8bfeb654ef54
Eyal Cidon <eyalcidon@gmail.com>
2018-04-17 15:17:48 -0700

    device only does local optimization

--- cartographer_ros/configuration_files/robot.lua
+++ cartographer_ros/configuration_files/robot.lua
@@ -16,5 +16,6 @@ include "map_builder_server.lua"
 
 MAP_BUILDER_SERVER.map_builder.use_trajectory_builder_2d = true
 MAP_BUILDER_SERVER.uplink_server_address = "localhost:55555"
+MAP_BUILDER_SERVER.map_builder.pose_graph.optimize_every_n_nodes = 0
 
 return MAP_BUILDER_SERVER
